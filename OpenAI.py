9fa09d8c5966c2d3f8b9506909588e3220bc9f4f
Bill Leece <bill.leece@gmail.com>
2023-05-03 09:21:54 -0500

    Update OpenAI.py
    Formatting fixes

--- OpenAI.py
+++ OpenAI.py
@@ -1,11 +1,8 @@
 import openai
 import time
 
-# Add your own organization and API keys here
-# Remember not to expose your API keys on Github (if you do they will be automatically invalidated) 
-
-openai.organization=$OpenAI-org-key # See https://platform.openai.com/account/org-settings 
-openai.api_key=$OpenAI-API-key # See https://platform.openai.com/account/api-keys 
+openai.organization = "org-2i9lWrQ8cRtI1wG0HtxcAV9e"
+openai.api_key = "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"
 
 # Set up a prompt and model engine
 prompt = input("\nAsk a question to ChatGPT here: ")
@@ -21,35 +18,37 @@ response = openai.Completion.create(
     temperature=0.8,
 )
 
-# Some analytics
+# Print the generated text
 generated_text = response.choices[0].text
-response_timestamp=time.time()
+response_timestamp = time.time()
 formatted_text = generated_text.replace(". ", ".\n")
 
-prompt_length=len(prompt)
-output_length=len(generated_text)
+prompt_length = len(prompt)
+output_length = len(generated_text)
 
-create_timestamp=response.get('created')
-process_time=(response_timestamp-create_timestamp)
+create_timestamp = response.get('created')
+process_time = (response_timestamp - create_timestamp)
+prompt_usage = response.get('usage').get('prompt_tokens')
+completion_usage = response.get('usage').get('completion_tokens')
+token_usage = response.get('usage').get('total_tokens')
+prompt_ratio = round(prompt_length / prompt_usage, 2)
+output_ratio = round(output_length / completion_usage, 2)
+total_ratio = round((prompt_length + output_length) / token_usage, 2)
+tt_ratio = round(completion_usage / process_time, 2)
 
-prompt_usage=response.get('usage').get('prompt_tokens')
-completion_usage=response.get('usage').get('completion_tokens')
-token_usage=response.get('usage').get('total_tokens')
-
-prompt_ratio=round(prompt_length/prompt_usage,2)
-output_ratio=round(output_length/completion_usage,2)
-total_ratio=round((prompt_length+output_length)/token_usage,2)
-tt_ratio=round(completion_usage/process_time,2)
-
-# Print the generated text
+# Some analytics
 print(f'\nThe length of the prompt text is', prompt_length, 'characters.')
 print(f'\nThe length of the output text is', output_length, 'characters.')
-print(f'\nThe total cost of this this operation is', token_usage,'tokens; the prompt consumed', prompt_usage, 'tokens, while the',
-        'output consumed', completion_usage, 'tokens.')
+print(f'\nThe total cost of this this operation is', token_usage, 'tokens; the prompt consumed', prompt_usage,
+      'tokens, while the',
+      'output consumed', completion_usage, 'tokens.')
 print(f'\nThe average prompt text length for one consumed prompt token is:', prompt_ratio, 'characters.')
 print(f'\nThe average output text length for one consumed output token is:', output_ratio, 'characters.')
 print(f'\nThe average total prompt + output text length for one consumed token is:', total_ratio, 'characters.')
 print(f'\nHere is the answer to your question from ChatGPT:')
 print(formatted_text)
-print(f'\n\nThe processing time required to generate the response was', round(process_time,2), 'seconds')
-print(f'\nThe ratio of tokens required to generate the response and the time (in seconds) taken to generate the response is', tt_ratio, 'tokens/second.')
+print(f'\n\nThe processing time required to generate the response was', round(process_time, 2), 'seconds')
+print(
+    f'\nThe ratio of tokens required to generate the response and the time (in seconds) taken to generate the '
+    f'response is',
+    tt_ratio, 'tokens/second.')
